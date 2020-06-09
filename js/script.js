de3914e9e7602b6b643483e9ff8b3991caf30b5d
Maqsudul Karim <62174598+Maqsudul@users.noreply.github.com>
2020-06-09 20:10:38 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+     
+    
+    $(window).scroll(function(){
+       var top = $(window).scrollTop();
+        if(top>=100){
+          $("nav").addClass('secondary'); 
+        }
+        else 
+            if($("nav").hasClass('secondary')){
+                $("nav").removeClass('secondary');  
+            }
+    });
+    
+ });
